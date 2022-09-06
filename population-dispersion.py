d0283e88b9507080c63c14d216ba266929fcdc0e
declayton <111589419+declayton@users.noreply.github.com>
2022-09-05 20:30:11 -0500

    Update population-dispersion.py

--- population-dispersion.py
+++ population-dispersion.py
@@ -1,1 +1,6 @@
-
+# Deanna Clayton mod3 File5
+## Calculate population variance and standard deviation
+import statistics
+rolls = [1, 3, 4, 2, 6, 5, 3, 4, 5, 2]
+print('Variance: ', statistics.pvariance(rolls))
+print('Standard Deviation: ', statistics.pstdev(rolls))
