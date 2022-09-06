a174b94109803879fba44b61e8fe2f34f580f0aa
declayton <111589419+declayton@users.noreply.github.com>
2022-09-05 19:50:18 -0500

    Update playing-craps.py

--- playing-craps.py
+++ playing-craps.py
@@ -1,7 +1,7 @@
 # Deanna Clayton mod3 File2
 ## Rolling 2 six sided dice
 import random
-## face frequency counters
+## set face frequency counters to 0
 frequency2 = 0
 frequency3 = 0
 frequency4 = 0
@@ -13,43 +13,48 @@ frequency9 = 0
 frequency10 = 0
 frequency11 = 0
 frequency12 = 0
-## 6,000,000 dice rolls
-for roll in range(6_000_000):
+## Deanna Clayton, Roll the dice 6,000,000 times
+for roll in range(6_000):
     face = random.randrange(1, 7) + random.randrange(1, 7)
     if face == 2:
         frequency2 += 1
-    elif face == 2:
-        frequency2 += 1
     elif face == 3:
-        frequency2 += 1
+        frequency3 += 1
     elif face == 4:
-        frequency2 += 1
+        frequency4 += 1
     elif face == 5:
-        frequency2 += 1
+        frequency5 += 1
     elif face == 6:
-        frequency2 += 1
+        frequency6 += 1
     elif face == 7:
-        frequency2 += 1
+        frequency7 += 1
     elif face == 8:
-        frequency2 += 1
+        frequency8 += 1
     elif face == 9:
-        frequency2 += 1
+        frequency9 += 1
     elif face == 10:
-        frequency2 += 1
+        frequency10 += 1
     elif face == 11:
-        frequency2 += 1
+        frequency11 += 1
     elif face == 12:
-        frequency2 += 1
-
+        frequency12 += 1
+## Deanna Clayton, Print the frequency of each roll combination
 print(f'Face{"Frequency":>13}')
 print(f'{2:>4}{frequency2:>13}')
-print(f'{3:>4}{frequency2:>13}')
-print(f'{4:>4}{frequency2:>13}')
-print(f'{5:>4}{frequency2:>13}')
-print(f'{6:>4}{frequency2:>13}')
-print(f'{7:>4}{frequency2:>13}')
-print(f'{8:>4}{frequency2:>13}')
-print(f'{9:>4}{frequency2:>13}')
-print(f'{10:>3}{frequency2:>13}')
-print(f'{11:>3}{frequency2:>13}')
-print(f'{12:>3}{frequency2:>13}')
+print(f'{3:>4}{frequency3:>13}')
+print(f'{4:>4}{frequency4:>13}')
+print(f'{5:>4}{frequency5:>13}')
+print(f'{6:>4}{frequency6:>13}')
+print(f'{7:>4}{frequency7:>13}')
+print(f'{8:>4}{frequency8:>13}')
+print(f'{9:>4}{frequency9:>13}')
+print(f'{10:>4}{frequency10:>13}')
+print(f'{11:>4}{frequency11:>13}')
+print(f'{12:>4}{frequency12:>13}')
+
+## Deanna Clayton, Rolling craps, or winning
+trials = 6_000
+craps = frequency2 + frequency3 + frequency12
+win = frequency7 + frequency11
+print('Craps:', craps/trials)
+print('Win:', win/trials)
