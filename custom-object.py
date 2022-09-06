ed6e37c0a506037abc0c8205d2567a1a1bc82f62
declayton <111589419+declayton@users.noreply.github.com>
2022-09-05 20:20:33 -0500

    Update custom-object.py

--- custom-object.py
+++ custom-object.py
@@ -1,1 +1,26 @@
+# Deanna Clayton mod3 File4
+## Create a Purchase object
+class Purchase(object):
+    def __init__(self, amount):
+        self.amount = amount
+    def calculateTax(self, taxPercent):
+        return self.amount * taxPercent/100.0
+    def calculateTip(self, tipPercent):
+        return self.amount * tipPercent/100.0
+    def calculateTotal(self, taxPercent, tipPercent):
+        return self.amount * (1 + taxPercent/100.0 + tipPercent/100.0)
 
+## Deanna Clayton, Give an amount to the purchase object
+## Set the tax and tip percentages
+purchase = Purchase(100.0)
+taxPercent = 7.5
+tipPercent = 20.0
+
+## Deanna Clayton, Calculate the tax and tip, and display the findings
+tax = purchase.calculateTax(taxPercent)
+tip = purchase.calculateTip(tipPercent)
+
+## Deanna Clayton, Display findings
+print('Tax: ', tax)
+print('Tip: ', tip)
+print('Total: ', purchase.calculateTotal(taxPercent, tipPercent))
